89e8918aa163f3b052a9ff409c27db16c914db08
Carter Carlson <carterbud1@gmail.com>
2018-12-04 15:54:34 -0600

    v1.0.0 - Better UI

--- 05_react-native/screens/ResultScreen.js
+++ 05_react-native/screens/ResultScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Text } from "native-base";
-import { Grid, Row } from "react-native-easy-grid";
+import { Dimensions, ClipBoard, StyleSheet } from "react-native";
+import { Container, Text, Button, Content } from "native-base";
 import { Constants } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Grid, Row } from "react-native-easy-grid";
 
 
 class ResultScreen extends Component {
@@ -10,17 +12,41 @@ class ResultScreen extends Component {
 		title: "Scan Details"
 	}
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			copied: false
+		};
+	}
+
+	copyToClipboard = content => {
+		ClipBoard.setString(content);
+		this.setState({ copied: true });
+	};
+
 	render() {
+		const { navigation } = this.props;
 		const qr = this.props.navigation.getParam("qr", "NO-QR");
+		let { height: screenHeight } = Dimensions.get("window");
+		screenHeight = screenHeight - 400;
 		return (
 			<Container style={{ marginTop: Constants.statusBarHeight }}>
 				<Grid
-					style={{
-						alignItems: "center"
-					}}
-				>
-					<Row>
-						<Text>{qr}</Text>
+					<Row style={styles.qrCard}>
+						<MaterialCommunityIcons name="qrcode" size={70} color="green" />
+						<Text style={styles.qrTitle}>{qr}</Text>
+					</Row>
+					<Row style={styles.copyButton}>
+						<Button
+							danger={!this.state.copied}
+							success={this.state.copied}
+							onPress={() => {
+								this.copyToClipboard(qr);
+							}}
+						>
+							<Text style={styles.copyButtonTitle}>Copy to Clipboard</Text>
+						</Button>
 					</Row>
 				</Grid>
 			</Container>
@@ -28,5 +54,25 @@ class ResultScreen extends Component {
 	}
 
 }
+
+const styles = StyleSheet.create({
+	qrCard: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "stretch",
+		margin: 10,
+		paddingHorizontal: 15,
+		paddingVertical: 10,
+		borderWidth: 1,
+		borderRadius: 10
+	},
+	qrTitle: { paddingTop: 10 },
+	copyButton: {
+		justifyContent: "center",
+		marginTop: 350
+	},
+	copyButtonTitle: { color: "white" }
+});
+
 
 export default ResultScreen;
--- 05_react-native/screens/ScannerScreen.js
+++ 05_react-native/screens/ScannerScreen.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Dimensions, StyleSheet } from "react-native";
-import { Container, Text, Button, Footer } from "native-base";
+import { Container, Text, View } from "native-base";
 import { Grid, Row } from "react-native-easy-grid";
 import { withNavigationFocus } from "react-navigation";
-
 import { BarCodeScanner, Permissions } from "expo";
+import { FontAwesome } from "@expo/vector-icons";
 
 import SQL from "../components/SQL";
 
@@ -38,49 +38,79 @@ class ScannerScreen extends Component {
 	};
 
 	render() {
-		const { hasCameraPermission } == this.state;
-		if (hasCameraPermission === null) {
-			return <Text>Requesting for camera permission</Text>
-		}
-		if (hasCameraPermission === false) {
-			return <Text>Unable to access camera</Text>
-		}
-		if (this.props.isFocused) {
+		const buttonColor = "green";
+		return this.state.hasCameraPermission === null ? (
+			<SpinnerScreen />
+		) : this.state.hasCameraPermission === false ? (
+			<Message>
+				<Text>No access to camera</Text>
+			</Message>
+		) : !this.state.scanned && this.props.isFocues ? (
+			<Container>
+				<BarCodeScanner
+					onBarCodeScanned={this.handleBarCodeScanned}
+					style={styles.BarCodeScanner}
+				>
+					<Grid>
+						<Row style={styles.layerTop} size={1} />
+						<Row style={styles.layerCenter} size={2} >
+							<View style={styles.layerLeft} />
+							<View style={styles.focused} />
+							<View style={styles.layerRight} />
+						</Row>
+						<Row style={styles.layerBottomRow} size={1}>
+							<FontAwesome
+								size={25}
+								name="history"
+								color={buttonColor}
+								onPress={() => {
+									this.props.navigation.navigate("History");
+								}}
+								style={styles.bottomButtons}
+							/>
+						</Row>
+					</Grid>
+				</BarCodeScanner>
+			</Container>
+		) : (
+			<SpinnerScreen />
+		);
 
-			return (
-				<Container>
-					<BarCodeScanner
-						onBarCodeScanned={this.handleBarCodeScanned}
-						style={styles.BarCodeScanner}
-					>
-						<Footer style={styles.layerBottom}>
-							<Grid>
-								<Row style={styles.layerBottomRow}>
-									<Button onPress={() => this.props.navigation.navigate("History")}>
-										<Text>History</Text>
-									</Button>
-								</Row>
-							</Grid>
-						</Footer>
-					</BarCodeScanner>
-			);
-		}
 	}
 
-	const styles = StyleSheet.create({
-		BarCodeScanner: {
-			height: Dimensions.get("window").height,
-			width: Dimensions.get("window").width,
-			flex: 1,
-			justifyContent: "flex-end"
-		},
-		layerBottom: { backgroundColor: "transparent" },
-		layerBottomRow: {
-			justifyContent: "center",
-			marginHorizontal: 50
-		}
-	});
-
 }
+
+const opacity = "rgba(0, 0, 0, .6)";
+
+const styles = StyleSheet.create({
+	BarCodeScanner: {
+		height: Dimensions.get("window").height,
+		width: Dimensions.get("window").width,
+		flex: 1,
+		justifyContent: "flex-end"
+	},
+	layerTop: { backgroundColor: opacity },
+	layerCenter: {
+		flex: 1.4,
+		flexDirection: "row"
+	},
+	layerLeft: {
+		flex: 6,
+		backgroundColor: opacity
+	},
+	focused: { flex: 30 },
+	layerRight: {
+		flex: 6,
+		backgroundColor: opacity
+	}
+	layerBottom: {
+		backgroundColor: opacity,
+		alignItems: "flex-end",
+		justifyContent: "center"
+	},
+	bottomButtons: { marginBottom: 40 }
+	}
+});
+
 
 export default ScannerScreen;
